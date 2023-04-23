8a773e002be459389a6b9039763672e328a21111
ahmed <Eng-Ahmed-abu-elBukhari>
2023-04-23 22:52:42 +0200

    maxler player

--- app/src/main/java/com/play/maxler/presentation/screens/folder/FoldersFragment.kt
+++ app/src/main/java/com/play/maxler/presentation/screens/folder/FoldersFragment.kt
@@ -12,11 +12,7 @@ import com.play.maxler.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FoldersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class FoldersFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
